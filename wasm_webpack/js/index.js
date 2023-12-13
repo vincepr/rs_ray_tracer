2ed5ab07de76b3f14848d44548df11cadcf04143
Vince <vinceprinceking@gmail.com>
2023-12-13 19:26:41 +0100

    added webpack project

---
+++ wasm_webpack/js/index.js
@@ -0,0 +1,20 @@
+// import("../pkg/index.js").catch(console.error);
+
+import('../pkg/index.js')
+    .then(wasm => {
+        const canvas = document.getElementById('drawing');
+        const ctx = canvas.getContext('2d');
+
+        const realInput = document.getElementById('real');
+        const imaginaryInput = document.getElementById('imaginary');
+        const renderBtn = document.getElementById('render');
+
+        renderBtn.addEventListener('click', () => {
+            const real = parseFloat(realInput.value) || 0;
+            const imaginary = parseFloat(imaginaryInput.value) || 0;
+            wasm.draw(ctx, 600, 1200, real, imaginary);
+        });
+
+        wasm.draw(ctx, 600, 600, -0.15, 0.65);
+    })
+    .catch(console.error);
